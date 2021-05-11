e9a36a39c92b5987f763586ae29136c679828eba
Meir Kriheli <meirkr@gmail.com>
2021-05-11 20:35:48 +0300

    products db

---
+++ Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    class CatalogContext : ICatalogContext
+    {
+        #region Implementation of ICatalogContext
+
+        public IMongoCollection<Product> ProductsCollection { get; }
+
+        #endregion
+
+        public CatalogContext(IConfiguration config)
+        {
+            var conString = config.GetValue<string>("DatabaseSettings.ConnectionString");
+            var dvNAme = config.GetValue<string>("DatabaseSettings.DatabaseName");
+            var collectionName = config.GetValue<string>("DatabaseSettings.CollectionName");
+            var client = new MongoClient(conString);
+            var database = client.GetDatabase(dvNAme);
+            
+            ProductsCollection = database.GetCollection<Product>(collectionName);
+            CatalogContextSeed.SeedData(ProductsCollection);
+        }
+    }
+}
